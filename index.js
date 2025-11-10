cd72f8d2c9f9eddf057027045d2c5e1f5ac49fd9
Mamun <mamunreza2025@gmail.com>
2025-11-10 18:49:13 +0600

    added a server side structure

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require("cors");
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello server connected')
+})
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
