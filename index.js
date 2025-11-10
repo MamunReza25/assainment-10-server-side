894f3b3b4aae0fccf5f70c4391a4ffe32082f0ff
Mamun <mamunreza2025@gmail.com>
2025-11-10 23:53:16 +0600

    added a get api show client side all data

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 const express = require('express');
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
 
 
+// 7HJPw4M4qAxWtIBb
+// assainment-10
 
+const uri = "mongodb+srv://assainment-10:7HJPw4M4qAxWtIBb@cluster0.5cco7jl.mongodb.net/?appName=Cluster0";
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // create mongobd database
+        const db = client.db("assainment_10_db");
+        const allCourseCollection = db.collection("all_course");
+
+        // all course data client side data show korar api
+        // find()
+        // findOne()
+        app.get('/allcourse', async (req, res) => {
+            const result = await allCourseCollection.find().toArray()
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello server connected')
